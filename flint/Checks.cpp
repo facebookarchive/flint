8f8ce8ac894729a57e207b7c877440393c143ed3
Joss Whittle <joss.whittle@l2program.co.uk>
2014-04-21 21:03:34 +0100

    Slowly adding in more of the check code

--- flint/Checks.cpp
+++ flint/Checks.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <set>
 #include <stack>
+#include <cassert>
 
 namespace flint {
 	
@@ -22,13 +23,135 @@ namespace flint {
 
 	void lintError(const Token &tok, const string &error) {
 		fprintf(stderr, "%.*s(%u): %s", tok.file_->c_str(), tok.line_, error.c_str());
-	}
+	};
 
 	void lintWarning(const Token &tok, const string &warning) {
 		lintError(tok, "Warning: " + warning);
-	}
+	};
 
 	void lintAdvice(const Token &tok, const string &advice) {
 		lintError(tok, "Advice: " + advice);
-	}
+	};
+
+	/**
+	* Traverses the token list until the whole template sequence has been passed...
+	* Returns with pos placed on the closing angle bracket
+	*
+	* @param tokens
+	*		The token list for the file
+	* @param pos
+	*		The current index position inside the token list
+	* @param containsArray
+	*		Optional parameter to return a bool of whether an array was found inside
+	*		the template list
+	*/
+	void skipTemplateSpec(vector<Token> &tokens, int &pos, bool *containsArray = nullptr) {
+		assert(tokens[pos].type_ == TK_LESS);
+
+		uint angleNest = 1; // Because we began on the leading '<'
+		uint parenNest = 0;
+
+		if (containsArray) {
+			*containsArray = false;
+		}
+
+		for (; tokens[pos].type_ != TK_EOF; ++pos) {
+			TokenType tok = tokens[pos].type_;
+
+			if (tok == TK_LPAREN) {
+				parenNest++;
+				continue;
+			}
+			if(tok == TK_RPAREN) {
+				parenNest--;
+				continue;
+			}
+
+			// Ignore angles inside of parens.  This avoids confusion due to
+			// integral template parameters that use < and > as comparison
+			// operators.
+			if (parenNest > 0) {
+				continue;
+			}
+
+			if (tok == TK_LSQUARE) {
+				if (angleNest == 1 && containsArray) {
+					*containsArray = true;
+				}
+				continue;
+			}
+
+			if (tok == TK_LESS) {
+				angleNest++;
+				continue;
+			}
+			if (tok == TK_GREATER) {
+				// Removed decrement/zero-check as one line
+				// It's not a race guys, readability > length of code
+				angleNest--;
+				if (angleNest == 0) {
+					break;
+				}
+				continue;
+			}
+		}
+	};
+
+	/**
+	* Returns whether the current token is a built in type
+	*
+	* @param tokens
+	*		The token list for the file
+	* @param pos
+	*		The current index position inside the token list
+	* @return
+	*		Returns true is the token as pos is a built in type
+	*/
+	bool atBuiltinType(vector<Token> &tokens, int &pos) {
+		
+		const vector<TokenType> builtIns = {
+			TK_DOUBLE,
+			TK_FLOAT,
+			TK_INT,
+			TK_SHORT,
+			TK_UNSIGNED,
+			TK_LONG,
+			TK_SIGNED,
+			TK_VOID,
+			TK_BOOL,
+			TK_WCHAR_T,
+			TK_CHAR
+		};
+
+		TokenType tok = tokens[pos].type_;
+		for (int i = 0; i < builtIns.size(); i++) {
+			if (tok == builtIns[i]) {
+				return true;
+			}
+		}
+		return false;
+	};
+	
+	/**
+	* Heuristically read a potentially namespace-qualified identifier,
+	* advancing 'pos' in the process.
+	*
+	* @param tokens
+	*		The token list for the file
+	* @param pos
+	*		The current index position inside the token list
+	* @return
+	*		Returns a vector of all the identifier values involved, or an
+	*		empty vector if no identifier was detected.
+	*/
+	vector<string> readQualifiedIdentifier(vector<Token> &tokens, int &pos) {
+		
+		vector<string> ret;
+		for (; tokens[pos].type_ == TK_IDENTIFIER || tokens[pos].type_ == TK_DOUBLE_COLON; ++pos) {
+			if (tokens[pos].type_ == TK_IDENTIFIER) {
+				ret.push_back(tokens[pos].value_);
+			}
+		}
+		return ret;
+	};
 };
