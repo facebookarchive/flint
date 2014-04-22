f9010bf75541c5330ee673c9d57e7ff772693d69
Joss Whittle <joss.whittle@l2program.co.uk>
2014-04-22 20:11:57 +0100

    Added try/catch and initial checks

--- flint/Main.cpp
+++ flint/Main.cpp
@@ -78,9 +78,20 @@ uint checkEntry(const string &path) {
 	file = removeIgnoredCode(file, path);
 
 	vector<Token> tokens;
-	tokenize(file, path, tokens);
-
 	
+	try {
+		tokenize(file, path, tokens);
+		
+		errors += checkBlacklistedSequences(path, tokens);
+
+		errors += checkInitializeFromItself(path, tokens);
+
+		if (!FLAGS_cmode) {
+			
+		}
+	} catch (exception const &e) {
+		fprintf(stderr, "Exception thrown during checks on %s.\n%s", path.c_str(), e.what());
+	}
 
 	return errors;
 };
