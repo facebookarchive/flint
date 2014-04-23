474783e0614044f843c3b5a2f93544ad7dcb52e4
Joss Whittle <joss.whittle@l2program.co.uk>
2014-04-23 16:53:42 +0100

    Added error level tracking

--- flint/Checks.hpp
+++ flint/Checks.hpp
@@ -9,9 +9,18 @@ using namespace std;
 
 namespace flint {
 
-#define X(func)																\
-	uint check##func(const string &path, const vector<Token> &tokens)
+	struct Errors {
+		uint errors;
+		uint warnings;
+		uint advice;
 
+		Errors() : errors(0), warnings(0), advice(0) {};
+	};
+
+#define X(func)																\
+	void check##func(Errors &errors, const string &path, const vector<Token> &tokens)
+
+	X(Iterators);
 	X(DefinedNames);
 	X(CatchByReference);
 	X(BlacklistedSequences);
