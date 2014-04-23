d40d424573ca020851eda92d10fda30d9a19b7ed
Joss Whittle <joss.whittle@l2program.co.uk>
2014-04-23 19:57:56 +0100

    Added non-compile test file to trigger checks

---
+++ flint/Test.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+
+// Define Warning Checks
+#define __BAD_DEFINE
+#define OKAY_DEFINE
+
+// Function Throw Checks
+void funcTest() throw () {};
+class Foo {
+private:
+	void Bar() throw () {};
+	void Bar2() {};
+};
+struct FooU {
+	void Bar() throw () {};
+};
+
+int main() {
+	
+	// Postfix Advice Checks
+	int i = 0;
+	i--;
+	++i;
+	for (i = 0; i < 10; i++) {}
+
+	// Try Catch Checks
+	try {
+
+	}
+	catch (int e) {
+
+	}
+
+	return 0;
+};
