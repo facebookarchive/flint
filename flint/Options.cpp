b9e71ef13162f94e30c9e2c89eb9a760e94ad201
Joss Whittle <joss.whittle@l2program.co.uk>
2014-05-23 16:47:20 +0100

    help formatting

--- flint/Options.cpp
+++ flint/Options.cpp
@@ -13,14 +13,14 @@ namespace flint {
 	void printHelp() {
 		printf("Usage: flint++ [options:] [files:]\n\n"
 			   "\t-r, --recursive		: Search subfolders for files.\n"
-			   "\t-c, --cmode		: Only perform C based lint checks.\n"
-			   "\t-j, --json		: Output report in JSON format.\n"
+			   "\t-c, --cmode			: Only perform C based lint checks.\n"
+			   "\t-j, --json			: Output report in JSON format.\n"
 			   "\t-v, --verbose		: Print full file paths.\n"
-			   "\t-l, --level [value: def = 3]	: Set the lint level.\n"
-			   "			      1 : Errors only\n"
-			   "			      2 : Errors & Warnings\n"
-			   "			      3 : All feedback\n\n"
-			   "\t-h, --help		: Print usage.\n\n");
+			   "\t-l, --level [def=3] : Set the lint level.\n"
+			   "			          1 : Errors only\n"
+			   "			          2 : Errors & Warnings\n"
+			   "			          3 : All feedback\n\n"
+			   "\t-h, --help		    : Print usage.\n\n");
 #ifdef _DEBUG 
 		// Stop visual studio from closing the window...
 		system("PAUSE");
