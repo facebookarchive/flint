d1f77a387f5edfaf49cb0e1b9b3824af3dac581d
Ben Mehne <bmehne@gmail.com>
2014-08-29 23:41:18 -0700

    Changed namespace for gflags functions to make compatible with default compilation options for the gflags project.

--- cxx/CxxReplace.cpp
+++ cxx/CxxReplace.cpp
@@ -32,7 +32,7 @@ struct CompareTokens : std::binary_function<Token, Token, bool> {
  * the command line.
  */
 int main(int argc, char ** argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
 
   if (argc <= 3) {
